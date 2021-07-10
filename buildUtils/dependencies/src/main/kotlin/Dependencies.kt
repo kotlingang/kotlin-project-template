06bb9f722eb607b0b7b099690f6b48d59392d080
Alex Sokol <sokolsokolinskiy@gmail.com>
2021-07-10 16:53:03 +0300

    Deps update

--- buildUtils/dependencies/src/main/kotlin/Dependencies.kt
+++ buildUtils/dependencies/src/main/kotlin/Dependencies.kt
@@ -6,7 +6,9 @@ object Deps {
     private const val serializationVersion = "1.2.2"
     private const val nodejsExternalsVersion = "0.0.7"
     private const val ktorVersion = "1.6.0"
+    private const val ktorOpenapiVersion = "0.2-beta.18"
     private const val exposedVersion = "0.32.1"
+    private const val datetimeVersion = "0.2.1"
 
     private const val postgresqlVersion = "42.2.23"
     private const val slf4jJVersion = "1.7.31"
@@ -15,24 +17,27 @@ object Deps {
 
     object Libs {
         object Kotlinx {
-            const val Coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${coroutinesVersion}"
-            const val Serialization = "org.jetbrains.kotlinx:kotlinx-serialization-json:${serializationVersion}"
-            const val Nodejs = "org.jetbrains.kotlinx:kotlinx-nodejs:${nodejsExternalsVersion}"
+            const val Coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion"
+            const val Serialization = "org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion"
+            const val Datetime = "org.jetbrains.kotlinx:kotlinx-datetime:$datetimeVersion"
+            const val Nodejs = "org.jetbrains.kotlinx:kotlinx-nodejs:$nodejsExternalsVersion"
         }
         object Ktor {
-            const val Core = "io.ktor:ktor-client-core:${ktorVersion}"
-            const val Cio = "io.ktor:ktor-client-cio:${ktorVersion}"
-            const val Serialization = "io.ktor:ktor-client-serialization:${ktorVersion}"
+            const val Core = "io.ktor:ktor-client-core:$ktorVersion"
+            const val Cio = "io.ktor:ktor-client-cio:$ktorVersion"
+            const val Serialization = "io.ktor:ktor-client-serialization:$ktorVersion"
+            const val Openapi = "com.github.papsign:Ktor-OpenAPI-Generator:$ktorOpenapiVersion"
         }
         object Exposed {
-            const val Core = "org.jetbrains.exposed:exposed-core:${exposedVersion}"
-            const val Jdbc = "org.jetbrains.exposed:exposed-jdbc:${exposedVersion}"
+            const val Core = "org.jetbrains.exposed:exposed-core:$exposedVersion"
+            const val Jdbc = "org.jetbrains.exposed:exposed-jdbc:$exposedVersion"
+            const val Time = "org.jetbrains.exposed:exposed-java-time:$exposedVersion"
         }
         object Postgres {
-            const val Driver = "org.postgresql:postgresql:${postgresqlVersion}"
+            const val Driver = "org.postgresql:postgresql:$postgresqlVersion"
         }
         object Slf4j {
-            const val Simple = "org.slf4j:slf4j-simple:${slf4jJVersion}"
+            const val Simple = "org.slf4j:slf4j-simple:$slf4jJVersion"
         }
     }
     object Plugins {
