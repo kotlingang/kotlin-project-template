b16542daf01819c9892305202849ae5ee7e8c1e7
Vadim Yaroschuk <y9vad9@gmail.com>
2022-05-08 20:22:47 +0300

    chore: kotlin version update

--- build-logic/dependencies/src/main/kotlin/Deps.kt
+++ build-logic/dependencies/src/main/kotlin/Deps.kt
@@ -4,7 +4,7 @@ object Deps {
     const val compileSdkVersion = 31
     const val minSdkVersion = 21
     
-    private const val kotlinVersion = "1.6.20"
+    private const val kotlinVersion = "1.7.0-Beta"
     private const val coroutinesVersion = "1.6.1"
     private const val serializationVersion = "1.3.2"
     private const val nodejsExternalsVersion = "0.0.7"
