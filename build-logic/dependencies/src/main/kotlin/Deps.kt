2607858f0c604722c71f4d53187d5459f51eb945
Vadim Yaroschuk <y9vad9@gmail.com>
2022-10-01 21:00:27 +0300

    chore: version updated

--- build-logic/dependencies/src/main/kotlin/Deps.kt
+++ build-logic/dependencies/src/main/kotlin/Deps.kt
@@ -4,13 +4,13 @@ object Deps {
     const val compileSdkVersion = 31
     const val minSdkVersion = 21
     
-    private const val kotlinVersion = "1.7.20-RC"
-    private const val coroutinesVersion = "1.6.2"
-    private const val serializationVersion = "1.3.3"
+    private const val kotlinVersion = "1.7.20"
+    private const val coroutinesVersion = "1.6.4"
+    private const val serializationVersion = "1.4.0"
     private const val nodejsExternalsVersion = "0.0.7"
-    private const val ktorVersion = "2.0.2"
+    private const val ktorVersion = "2.1.2"
     private const val ktorOpenapiVersion = "0.2-beta.20"
-    private const val exposedVersion = "0.35.1"
+    private const val exposedVersion = "0.39.2"
     private const val datetimeVersion = "0.3.0"
 
     private const val postgresqlVersion = "42.2.24"
