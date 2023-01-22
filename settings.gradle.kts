ca17d5a64365f88b1ee2e9e6f6227257cfc5f016
Vadim Yaroschuk <y9vad9@gmail.com>
2023-01-22 20:14:35 +0200

    [REFACTOR]: version catalog & project accessors

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,25 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+enableFeaturePreview("VERSION_CATALOGS")
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
+        mavenCentral()
         google()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://maven.y9vad9.com")
     }
 }
 
 rootProject.name = "kotlin-project-template"
 
-includeBuild("buildUtils/dependencies")
-includeBuild("buildUtils/configuration")
+//includeBuild("buildUtils/service-deploy")
 //includeBuild("buildUtils/library-deploy")
